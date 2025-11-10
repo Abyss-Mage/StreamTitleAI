e0056540e72271a74770888b1d81670b29944ab0
abyss-mage <guhaanirban01@gmail.com>
2025-11-10 13:07:50 +0530

    modified:   frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     proxy: {
       // Proxies any request starting with /api to the backend
       '/api': {
-        target: 'https://backend.streamtitle.abyssmage.fun/', // Your backend server address
+        target: 'http://localhost:3001', // Your backend server address
         changeOrigin: true, // Recommended
         secure: false, // Set to false for http
       }
